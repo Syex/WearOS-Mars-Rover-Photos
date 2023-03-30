fd12adb29d73c937f63336b3508118b4ab681bf6
Tom Seifert <dev@tpseifert.de>
2023-03-30 19:34:05 +0200

    Add API calls to fetch rover photos

--- app/src/main/java/de/memorian/wearos/marsrover/data/MarsRoverApi.kt
+++ app/src/main/java/de/memorian/wearos/marsrover/data/MarsRoverApi.kt
@@ -1,6 +1,7 @@
 package de.memorian.wearos.marsrover.data
 
 import de.memorian.wearos.marsrover.data.entity.MissionManifestEntity
+import de.memorian.wearos.marsrover.data.entity.RoverPhotoEntity
 import de.memorian.wearos.marsrover.di.ApiKey
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -9,29 +10,57 @@ import javax.inject.Inject
 
 private const val BASE_URL = "https://api.nasa.gov/mars-photos/api/v1"
 private const val MANIFEST_URL = "$BASE_URL/manifests"
+private const val ROVERS_URL = "$BASE_URL/rovers"
+
+private const val PATH_CURIOSITY = "curiosity"
+private const val PATH_OPPORTUNITY = "opportunity"
+private const val PATH_SPIRIT = "spirit"
 
 private const val FIELD_API_KEY = "api_key"
+private const val FIELD_SOL = "sol"
+private const val FIELD_CAMERA = "camera"
 
 class MarsRoverApi @Inject constructor(
     private val client: HttpClient,
     @ApiKey private val apiKey: String,
 ) {
 
-    suspend fun getCuriosityMissionManifest(): MissionManifestEntity {
-        return client.get("$MANIFEST_URL/curiosity") {
+    suspend fun getCuriosityMissionManifest(): MissionManifestEntity = getMissionManifest(
+        PATH_CURIOSITY
+    )
+
+    suspend fun getOpportunityMissionManifest(): MissionManifestEntity = getMissionManifest(
+        PATH_OPPORTUNITY
+    )
+
+    suspend fun getSpiritMissionManifest(): MissionManifestEntity = getMissionManifest(
+        PATH_SPIRIT
+    )
+
+    private suspend fun getMissionManifest(path: String): MissionManifestEntity {
+        return client.get("$MANIFEST_URL/$path") {
             parameter(FIELD_API_KEY, apiKey)
         }.body()
     }
 
-    suspend fun getOpportunityMissionManifest(): MissionManifestEntity {
-        return client.get("$MANIFEST_URL/opportunity") {
-            parameter(FIELD_API_KEY, apiKey)
-        }.body()
-    }
+    suspend fun getCuriosityPhotos(sol: Int, camera: String?): List<RoverPhotoEntity> =
+        getRoverPhotos(PATH_CURIOSITY, sol, camera)
 
-    suspend fun getSpiritMissionManifest(): MissionManifestEntity {
-        return client.get("$MANIFEST_URL/spirit") {
+    suspend fun getOpportunityPhotos(sol: Int, camera: String?): List<RoverPhotoEntity> =
+        getRoverPhotos(PATH_OPPORTUNITY, sol, camera)
+
+    suspend fun getSpiritPhotos(sol: Int, camera: String?): List<RoverPhotoEntity> =
+        getRoverPhotos(PATH_SPIRIT, sol, camera)
+
+    private suspend fun getRoverPhotos(
+        path: String,
+        sol: Int,
+        camera: String?,
+    ): List<RoverPhotoEntity> {
+        return client.get("$ROVERS_URL/$path") {
             parameter(FIELD_API_KEY, apiKey)
+            parameter(FIELD_SOL, sol)
+            camera?.let { parameter(FIELD_CAMERA, it) }
         }.body()
     }
 }
