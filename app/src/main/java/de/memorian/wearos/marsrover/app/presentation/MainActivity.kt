c8d6fbef5218e80fab8767cd3a16ba3d3530b4da
Tom Seifert <dev@tpseifert.de>
2023-04-02 17:49:57 +0200

    Add launcher icons

--- app/src/main/java/de/memorian/wearos/marsrover/app/presentation/MainActivity.kt
+++ app/src/main/java/de/memorian/wearos/marsrover/app/presentation/MainActivity.kt
@@ -1,9 +1,3 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
- * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
- * changes to the libraries and their usages.
- */
-
 package de.memorian.wearos.marsrover.app.presentation
 
 import android.os.Bundle
@@ -13,17 +7,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
 import dagger.hilt.android.AndroidEntryPoint
-import de.memorian.wearos.marsrover.R
 import de.memorian.wearos.marsrover.app.presentation.theme.MarsRoverTheme
 
 @AndroidEntryPoint
@@ -49,20 +36,4 @@ fun WearApp() {
             NavHost()
         }
     }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp()
 }
