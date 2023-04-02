89b455d9902cad9b16957c6d28d03d991e5ce7b7
Tom Seifert <dev@tpseifert.de>
2023-04-02 14:35:38 +0200

    Update theme

--- app/src/main/java/de/memorian/wearos/marsrover/app/presentation/theme/Theme.kt
+++ app/src/main/java/de/memorian/wearos/marsrover/app/presentation/theme/Theme.kt
@@ -1,7 +1,35 @@
 package de.memorian.wearos.marsrover.app.presentation.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.wear.compose.material.Colors
 import androidx.wear.compose.material.MaterialTheme
+
+val md_theme_primary = Color(0xFF924C00)
+val md_theme_onPrimary = Color(0xFFFFFFFF)
+val md_theme_secondary = Color(0xFF845400)
+val md_theme_onSecondary = Color(0xFFFFFFFF)
+val md_theme_error = Color(0xFFBA1A1A)
+val md_theme_onError = Color(0xFFFFFFFF)
+val md_theme_background = Color(0xFF451804)
+val md_theme_onBackground = Color(0xFF201A17)
+val md_theme_surface = Color(0xFF451804)
+val md_theme_onSurface = Color(0xFF201A17)
+val md_theme_onSurfaceVariant = Color(0xFF52443B)
+
+private val wearColorPalette = Colors(
+    primary = md_theme_primary,
+    onPrimary = md_theme_onPrimary,
+    secondary = md_theme_secondary,
+    onSecondary = md_theme_onSecondary,
+    error = md_theme_error,
+    onError = md_theme_onError,
+    background = md_theme_background,
+    onBackground = md_theme_onBackground,
+    surface = md_theme_surface,
+    onSurface = md_theme_onSurface,
+    onSurfaceVariant = md_theme_onSurfaceVariant,
+)
 
 @Composable
 fun MarsRoverTheme(
@@ -9,9 +37,6 @@ fun MarsRoverTheme(
 ) {
     MaterialTheme(
         colors = wearColorPalette,
-        typography = Typography,
-        // For shapes, we generally recommend using the default Material Wear shapes which are
-        // optimized for round and non-round devices.
         content = content
     )
 }
