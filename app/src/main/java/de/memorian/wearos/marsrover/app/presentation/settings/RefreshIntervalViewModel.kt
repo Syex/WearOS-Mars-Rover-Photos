cbbe2b50e06af3b939aa7ca6506e2b5983594bcd
Tom Seifert <dev@tpseifert.de>
2023-04-04 17:12:33 +0200

    Launch job to persist new interval as NonCancellable

--- app/src/main/java/de/memorian/wearos/marsrover/app/presentation/settings/RefreshIntervalViewModel.kt
+++ app/src/main/java/de/memorian/wearos/marsrover/app/presentation/settings/RefreshIntervalViewModel.kt
@@ -6,6 +6,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import de.memorian.wearos.marsrover.app.data.persistence.SettingsStore
 import de.memorian.wearos.marsrover.app.presentation.settings.RefreshIntervalScreenState.ShowPicker
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -31,7 +32,7 @@ class RefreshIntervalViewModel(
         _screenStateFlow.emit(ShowPicker(refreshInterval))
     }
 
-    fun onNewIntervalConfirmed(newInterval: Int) = coroutineScope.launch {
+    fun onNewIntervalConfirmed(newInterval: Int) = coroutineScope.launch(NonCancellable) {
         settingsStore.storeNewRefreshInterval(newInterval)
     }
 }
